cb1888a7f4730d3154f544de8444dbc95d839732
ifi999 <kkwkjm4231@gmail.com>
2023-01-07 17:24:45 +0900

    add: 공용 Entity 추가

---
+++ src/main/java/ifi/realworld/common/entity/BaseCreateInfoEntity.java
@@ -0,0 +1,26 @@
+package ifi.realworld.common.entity;
+
+import lombok.Getter;
+import org.springframework.data.annotation.CreatedBy;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import javax.persistence.Column;
+import javax.persistence.EntityListeners;
+import javax.persistence.MappedSuperclass;
+import java.time.LocalDateTime;
+
+@Getter
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+public class BaseCreateInfoEntity {
+
+    @CreatedBy
+    @Column(updatable = false)
+    private Long createdBy;
+
+    @CreatedDate
+    @Column(updatable = false)
+    private LocalDateTime createdAt;
+
+}
---
+++ src/main/java/ifi/realworld/common/entity/BaseUpdateInfoEntity.java
@@ -0,0 +1,24 @@
+package ifi.realworld.common.entity;
+
+import lombok.Getter;
+import org.springframework.data.annotation.LastModifiedBy;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import javax.persistence.Column;
+import javax.persistence.EntityListeners;
+import javax.persistence.MappedSuperclass;
+import java.time.LocalDateTime;
+
+@Getter
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+public class BaseUpdateInfoEntity extends BaseCreateInfoEntity {
+
+    @LastModifiedBy
+    private Long lastModifiedBy;
+
+    @LastModifiedDate
+    private LocalDateTime lastModifiedAt;
+
+}
