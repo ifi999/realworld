55d9c4495da578e7207b606851e0b81852ff5ae3
ifi999 <kkwkjm4231@gmail.com>
2023-01-07 17:30:56 +0900

    fix: Serializable 상속

--- src/main/java/ifi/realworld/common/entity/BaseCreateInfoEntity.java
+++ src/main/java/ifi/realworld/common/entity/BaseCreateInfoEntity.java
@@ -8,12 +8,15 @@ import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 import javax.persistence.Column;
 import javax.persistence.EntityListeners;
 import javax.persistence.MappedSuperclass;
+import java.io.Serializable;
 import java.time.LocalDateTime;
 
 @Getter
 @MappedSuperclass
 @EntityListeners(AuditingEntityListener.class)
-public class BaseCreateInfoEntity {
+public class BaseCreateInfoEntity implements Serializable {
+
+    private static final long serialVersionUID = -4575268096250943411L;
 
     @CreatedBy
     @Column(updatable = false)
