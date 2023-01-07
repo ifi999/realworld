1c429c58bb092a9dc7a3df8fdd295bb2fa836d73
ifi999 <kkwkjm4231@gmail.com>
2023-01-08 00:21:47 +0900

    refactor: method 추출, EntityManager 사용 추가

--- src/test/java/ifi/realworld/user/api/UserControllerTest.java
+++ src/test/java/ifi/realworld/user/api/UserControllerTest.java
@@ -1,16 +1,17 @@
 package ifi.realworld.user.api;
 
-import ifi.realworld.user.domain.repository.UserRepository;
+import ifi.realworld.user.app.service.UserService;
 import ifi.realworld.user.domain.User;
+import ifi.realworld.user.domain.repository.UserRepository;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.test.annotation.Rollback;
 import org.springframework.transaction.annotation.Transactional;
 
-import static org.assertj.core.api.Assertions.*;
+import javax.persistence.EntityManager;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 @Transactional
 @SpringBootTest
@@ -20,13 +21,20 @@ class UserControllerTest {
     UserRepository userRepository;
 
     @Autowired
+    UserService userService;
+
+    @Autowired
+    EntityManager em;
+
+    @Autowired
     UserPasswordEncoder passwordEncoder;
 
+    // TODO - 테스트 뭔가 잘못된듯.. service, dto 사용해야함
     @DisplayName("유저 등록")
     @Test
     public void createUser() {
         //given
-        User user = getUser();
+        User user = userBuilder();
 
         //when
         User result = userRepository.save(user);
@@ -36,13 +44,34 @@ class UserControllerTest {
         assertThat(user.getUsername()).isEqualTo(result.getUsername());
     }
 
-    private User getUser() {
+    private User userBuilder() {
         return User.builder()
                 .email("test@rw.com")
                 .username("hi")
                 .password("1234")
                 .passwordEncoder(passwordEncoder)
                 .build();
+    }
+
+    @DisplayName("로그인")
+    @Test
+    public void login() {
+        //given
+        User user = setUser();
+        em.flush();
+        em.clear();
+
+
+        //when
+//        User login = userService.login(dto);
+
+        //then
+//        assertThat(user.getUsername()).isEqualTo(login.getUsername());
+
+    }
+
+    private User setUser() {
+        return userRepository.save(userBuilder());
     }
 
 }
