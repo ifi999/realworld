1d86bb7d8a9bfe080940ed8361d7c27c49426d92
ifi999 <kkwkjm4231@gmail.com>
2023-01-07 18:37:15 +0900

    add: Security 설정

---
+++ src/main/java/ifi/realworld/common/config/SecurityConfig.java
@@ -0,0 +1,44 @@
+package ifi.realworld.common.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                    .authorizeRequests()
+                        .antMatchers(HttpMethod.GET, "/test", "/login").permitAll()
+                        .anyRequest().authenticated()
+                .and()
+                    .csrf().disable()
+                    .formLogin().disable();
+
+        return http.build();
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+        PasswordEncoder encoder = new BCryptPasswordEncoder();
+
+        UserDetails user = User.builder()
+                .passwordEncoder(encoder::encode)
+                .username("username")
+                .password("password")
+                .roles("USER") // TODO - 유저 권한 나누기?
+                .build();
+
+        return new InMemoryUserDetailsManager(user);
+    }
+}
