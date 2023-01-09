af382b141555f4294379653a1051c84049280f69
ifi999 <kkwkjm4231@gmail.com>
2023-01-09 11:45:30 +0900

    add: USER_NOT_FOUND 추가

--- src/main/java/ifi/realworld/common/exception/ErrorCode.java
+++ src/main/java/ifi/realworld/common/exception/ErrorCode.java
@@ -3,8 +3,8 @@ package ifi.realworld.common.exception;
 public enum ErrorCode {
     ALREADY_EXISTED_USER,
     PASSWORD_NOT_MATCHED,
-    INVALID_EMAIL
+    INVALID_EMAIL,
+    USER_NOT_FOUND
     ;
-
 
 }
