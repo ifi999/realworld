5e5c66addb0ea6e4a06546e95607acaf881df6c2
ifi999 <kkwkjm4231@gmail.com>
2023-01-10 11:50:25 +0900

    add: follow Entity 추가

---
+++ src/main/java/ifi/realworld/user/domain/FollowRelation.java
@@ -0,0 +1,26 @@
+package ifi.realworld.user.domain;
+
+import ifi.realworld.common.entity.BaseCreateInfoEntity;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.EmbeddedId;
+import javax.persistence.Entity;
+
+@Getter
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class FollowRelation extends BaseCreateInfoEntity {
+
+    @EmbeddedId
+    private FollowRelationId followRelationId;
+
+    public FollowRelation(FollowRelationId followRelationId) {
+        this.followRelationId = followRelationId;
+    }
+
+    public FollowRelation(long followerId, long followeeId) {
+        this.followRelationId = new FollowRelationId(followerId, followeeId);
+    }
+}
---
+++ src/main/java/ifi/realworld/user/domain/FollowRelationId.java
@@ -0,0 +1,24 @@
+package ifi.realworld.user.domain;
+
+import lombok.*;
+
+import javax.persistence.Embeddable;
+import java.io.Serializable;
+
+@Getter
+@ToString
+@Embeddable
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class FollowRelationId implements Serializable {
+
+    private Long followerId;
+
+    private Long followeeId;
+
+    @Builder
+    public FollowRelationId(Long followerId, Long followeeId) {
+        this.followerId = followerId;
+        this.followeeId = followeeId;
+    }
+
+}
