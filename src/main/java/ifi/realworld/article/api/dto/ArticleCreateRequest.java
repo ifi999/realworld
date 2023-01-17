62fabfc6423133338731ccbf58c6ee0f79bee66e
ifi999 <kkwkjm4231@gmail.com>
2023-01-17 14:24:55 +0900

    fix: dto validation 추가

--- src/main/java/ifi/realworld/article/api/dto/ArticleCreateRequest.java
+++ src/main/java/ifi/realworld/article/api/dto/ArticleCreateRequest.java
@@ -1,16 +1,24 @@
 package ifi.realworld.article.api.dto;
 
 import lombok.Getter;
+import org.hibernate.validator.constraints.Length;
 
+import javax.validation.constraints.NotEmpty;
 import java.util.List;
 
 @Getter
 public class ArticleCreateRequest {
 
+    @NotEmpty
+    @Length(max = 255)
     private String title;
 
+    @NotEmpty
+    @Length(max = 255)
     private String description;
 
+    @NotEmpty
+    @Length(max = 3000)
     private String body;
 
     private List<String> tagList;
