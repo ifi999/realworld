d84e4352c1ce06d235f4a9f111df11b9101602d9
ifi999 <kkwkjm4231@gmail.com>
2023-01-19 15:34:52 +0900

    add: Favorite Entity 수정 - field 명칭, 불필요 field 삭제

--- src/main/java/ifi/realworld/favorite/domain/Favorite.java
+++ src/main/java/ifi/realworld/favorite/domain/Favorite.java
@@ -3,6 +3,7 @@ package ifi.realworld.favorite.domain;
 import ifi.realworld.article.domain.Article;
 import ifi.realworld.user.domain.User;
 import lombok.AccessLevel;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
@@ -17,15 +18,17 @@ public class Favorite {
     @GeneratedValue
     @Column(name = "favorite_id")
     private Long id;
-
-    private FavoriteType type;
-
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "user_id")
-    private User userId;
+    private User user;
 
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "article_id")
-    private Article articleId;
+    private Article article;
 
+    @Builder
+    public Favorite(User userId, Article articleId) {
+        this.user = userId;
+        this.article = articleId;
+    }
 }
