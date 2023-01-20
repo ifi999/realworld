443c8786e890bc2bcf60cf87d24e0658741705b7
ifi999 <kkwkjm4231@gmail.com>
2023-01-20 11:59:30 +0900

    fix: 요구 spec 반영

--- src/main/java/ifi/realworld/comment/api/dto/CommentResponseDto.java
+++ src/main/java/ifi/realworld/comment/api/dto/CommentResponseDto.java
@@ -1,13 +1,24 @@
 package ifi.realworld.comment.api.dto;
 
+import ifi.realworld.comment.domain.Comment;
 import lombok.Getter;
+
+import java.time.LocalDateTime;
 
 @Getter
 public class CommentResponseDto {
 
-    String body;
+    private Long id;
+    private String body;
+    private LocalDateTime createAt;
+    private LocalDateTime updateAt;
+    // TODO - createBy 필요
+//    private User author;
 
-    public CommentResponseDto(String body) {
-        this.body = body;
+    public CommentResponseDto(Comment comment) {
+        this.id = comment.getId();
+        this.body = comment.getBody();
+        this.createAt = comment.getCreatedAt();
+        this.updateAt = comment.getLastModifiedAt();
     }
 }
