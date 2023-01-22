b9939345ad1301fca9e4e3309c68a913c12b364b
ifi999 <kkwkjm4231@gmail.com>
2023-01-22 22:11:49 +0900

    add: lastModifiedBy 자동 입력값 설정

--- src/main/java/ifi/realworld/common/entity/BaseUpdateInfoEntity.java
+++ src/main/java/ifi/realworld/common/entity/BaseUpdateInfoEntity.java
@@ -6,7 +6,6 @@ import org.springframework.data.annotation.LastModifiedBy;
 import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
-import javax.persistence.Column;
 import javax.persistence.EntityListeners;
 import javax.persistence.MappedSuperclass;
 import java.time.LocalDateTime;
@@ -17,7 +16,7 @@ import java.time.LocalDateTime;
 public class BaseUpdateInfoEntity extends BaseCreateInfoEntity {
 
     @LastModifiedBy
-    private Long lastModifiedBy;
+    private String lastModifiedBy;
 
     @LastModifiedDate
     @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm:ss", timezone = "Asia/Seoul")
