3afa20bc52d3fb8d2bdee475dac7f12dd07dce00
ifi999 <kkwkjm4231@gmail.com>
2023-02-19 23:20:28 +0900

    test: getUserTest() 추가 - jwt cookie 쿠키 인증

--- src/test/java/ifi/realworld/user/UserControllerTest.java
+++ src/test/java/ifi/realworld/user/UserControllerTest.java
@@ -3,17 +3,30 @@ package ifi.realworld.user;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import ifi.realworld.user.api.dto.UserCreateRequest;
 import ifi.realworld.user.api.dto.UserLoginDto;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseCookie;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.util.ReflectionTestUtils;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.transaction.annotation.Transactional;
 
+import javax.servlet.http.Cookie;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.atomic.AtomicInteger;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -26,6 +39,11 @@ class UserControllerTest {
 
     @Autowired
     MockMvc mvc;
+
+    @Value("${jwt.secret}")
+    String secret;
+
+    private final SignatureAlgorithm signatureAlgorithm = SignatureAlgorithm.HS256;
 
     @Test
     public void createUserTest() throws Exception {
@@ -57,5 +75,40 @@ class UserControllerTest {
                 .andExpect(status().isOk())
                 .andExpect(jsonPath("$.email").exists())
                 .andExpect(jsonPath("$.email").value(request.getEmail()));
+    }
+
+    @Test
+    public void getUserTest() throws Exception {
+        Cookie cookie = new Cookie("Authorization", createToken());
+        cookie.setMaxAge(1800 * 1000);
+        cookie.setPath("/");
+
+        mvc.perform(
+                get("/api/users")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .cookie(cookie)
+        )
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.email").exists());
+    }
+
+    private String createToken() {
+        Date now = new Date();
+        Date expiration = new Date(now.getTime() + 1800 * 1000);
+
+        Map<String, Object> headerMap = new HashMap<>();
+        headerMap.put("typ", "JWT");
+        headerMap.put("alg", "HS256");
+
+        Map<String, Object> claims = new HashMap<>();
+        claims.put("email", "bye@hel.lo");
+
+        return Jwts.builder()
+                .setHeader(headerMap)
+                .setClaims(claims)
+                .signWith(Keys.hmacShaKeyFor(Decoders.BASE64.decode(secret)), signatureAlgorithm)
+                .setIssuedAt(now)
+                .setExpiration(expiration)
+                .compact();
     }
 }
