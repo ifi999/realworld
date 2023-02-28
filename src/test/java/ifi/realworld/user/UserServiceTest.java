2f8ea1a7dfa9ee3416ea8a568cb11cb729985423
ifi999 <kkwkjm4231@gmail.com>
2023-02-28 22:50:28 +0900

    test: UserServiceTest 수정

--- src/test/java/ifi/realworld/user/UserServiceTest.java
+++ src/test/java/ifi/realworld/user/UserServiceTest.java
@@ -1,5 +1,7 @@
 package ifi.realworld.user;
 
+import ifi.realworld.common.security.CustomUserDetailsService;
+import ifi.realworld.common.security.jwt.JwtProvider;
 import ifi.realworld.user.api.UserPasswordEncoder;
 import ifi.realworld.user.api.dto.UserCreateRequest;
 import ifi.realworld.user.api.dto.UserCreateResponse;
@@ -8,26 +10,45 @@ import ifi.realworld.user.app.service.UserServiceImpl;
 import ifi.realworld.user.domain.User;
 import ifi.realworld.user.domain.repository.UserRepository;
 import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.util.ReflectionTestUtils;
+import org.springframework.transaction.annotation.Transactional;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-@ExtendWith(MockitoExtension.class)
+@Transactional
+@SpringBootTest
 public class UserServiceTest {
 
-    @InjectMocks
-    private UserServiceImpl userService;
-
-    @Mock
+    @Autowired
     private UserRepository userRepository;
 
     @Mock
     private UserPasswordEncoder userPasswordEncoder;
+
+    @Mock
+    private JwtProvider jwtProvider;
+
+    @Mock
+    private CustomUserDetailsService userDetailsService;
+
+    private String header;
+
+    private long validitySeconds;
+
+    private UserService userService;
+
+    @BeforeEach
+    void setUp() {
+        userService = new UserServiceImpl(userRepository, userPasswordEncoder, jwtProvider, userDetailsService, header, validitySeconds);
+    }
 
     @Test
     public void createUser_Success_test() {
@@ -42,7 +63,6 @@ public class UserServiceTest {
 
         //then
         assertThat(response.getEmail()).isEqualTo(request.getEmail());
-
     }
 
 }
