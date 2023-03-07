1c576b5ee16a2347eba13dad7cb5cec8cd343bb2
ifi999 <kkwkjm4231@gmail.com>
2023-03-07 10:04:21 +0900

    refactor: UserAuthenticationException 상속 RuntimeException으로 수정

--- src/main/java/ifi/realworld/common/exception/UserAuthenticationException.java
+++ src/main/java/ifi/realworld/common/exception/UserAuthenticationException.java
@@ -1,6 +1,6 @@
 package ifi.realworld.common.exception;
 
-public class UserAuthenticationException extends DefaultCustomException {
+public class UserAuthenticationException extends RuntimeException {
     private static final long serialVersionUID = 4076256550380142367L;
 
     public UserAuthenticationException(String message, Throwable cause) {
