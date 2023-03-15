a5c436b0877dff9641b21b7ae7879f5c75c17189
ifi999 <kkwkjm4231@gmail.com>
2023-03-15 17:43:18 +0900

    refactor: PasswordEncoder 중복 제거

--- src/main/java/ifi/realworld/user/api/UserPasswordEncoder.java
+++ src/main/java/ifi/realworld/user/api/UserPasswordEncoder.java
@@ -1,12 +1,8 @@
 package ifi.realworld.user.api;
 
-import org.springframework.security.crypto.password.PasswordEncoder;
+public interface UserPasswordEncoder {
 
-public interface UserPasswordEncoder extends PasswordEncoder {
-
-    @Override
     String encode(CharSequence rawPassword);
 
-    @Override
     boolean matches(CharSequence rawPassword, String encodedPassword);
 }
