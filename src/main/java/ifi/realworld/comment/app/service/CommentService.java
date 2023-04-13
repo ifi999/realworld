419c1aeea5093e7ff47cec7b849e7edc657bd741
ifi999 <kkwkjm4231@gmail.com>
2023-04-13 22:49:47 +0900

    refactor: Comment Service Interface 제거

--- src/main/java/ifi/realworld/comment/app/service/CommentService.java
+++ src/main/java/ifi/realworld/comment/app/service/CommentService.java
@@ -1,15 +1,54 @@
 package ifi.realworld.comment.app.service;
 
+import ifi.realworld.article.domain.Article;
+import ifi.realworld.article.domain.repository.ArticleRepository;
 import ifi.realworld.comment.api.dto.CommentCreateRequest;
 import ifi.realworld.comment.api.dto.CommentResponseDto;
+import ifi.realworld.comment.domain.Comment;
+import ifi.realworld.comment.domain.repository.CommentRepository;
+import ifi.realworld.common.exception.api.ArticleNotFoundException;
+import ifi.realworld.common.exception.api.NotFoundCommentException;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
-public interface CommentService {
+@Service
+@Transactional
+@RequiredArgsConstructor
+public class CommentService {
 
-    CommentResponseDto createComments(String slug, CommentCreateRequest dto);
+    private final CommentRepository commentRepository;
+    private final ArticleRepository articleRepository;
 
-    List<CommentResponseDto> getComments(String slug);
+    public CommentResponseDto createComments(String slug, CommentCreateRequest dto) {
+        Article article = getArticle(slug);
+        Comment savedComment = commentRepository.save(new Comment(dto.getBody(), article));
 
-    void deleteComments(String slug, Long id);
+        return new CommentResponseDto(savedComment);
+    }
+
+    public List<CommentResponseDto> getComments(String slug) {
+        Article article = getArticle(slug);
+        List<Comment> commentList = commentRepository.findByArticleId(article.getId());
+        List<CommentResponseDto> result = new ArrayList<>();
+        commentList.stream()
+                .forEach(o -> result.add(new CommentResponseDto(o)));
+        return result;
+    }
+
+    public void deleteComments(String slug, Long id) {
+        Article article = getArticle(slug);
+        Optional<Comment> findComment = commentRepository.findByIdAndArticleId(id, article);
+        if (findComment.isEmpty()) throw new NotFoundCommentException("This " + slug + " doesn't have this comment id " + id);
+
+        commentRepository.deleteByIdAndArticleId(id, article);
+    }
+
+    private Article getArticle(String slug) {
+        return articleRepository.findBySlug(slug).orElseThrow(ArticleNotFoundException::new);
+    }
 }
