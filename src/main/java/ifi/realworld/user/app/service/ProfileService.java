ff5bb5fd260cc6fd78420e4ebc07267d3ba165d8
ifi999 <kkwkjm4231@gmail.com>
2023-04-13 22:50:07 +0900

    refactor: Profile Service Interface 제거

--- src/main/java/ifi/realworld/user/app/service/ProfileService.java
+++ src/main/java/ifi/realworld/user/app/service/ProfileService.java
@@ -1,12 +1,56 @@
 package ifi.realworld.user.app.service;
 
+import ifi.realworld.common.exception.api.UserNotFoundException;
 import ifi.realworld.user.api.dto.ProfileDto;
+import ifi.realworld.user.domain.FollowRelation;
+import ifi.realworld.user.domain.User;
+import ifi.realworld.user.domain.repository.FollowJpaRepository;
+import ifi.realworld.user.domain.repository.FollowRepository;
+import ifi.realworld.user.domain.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
-public interface ProfileService {
+@Slf4j
+@Service
+@Transactional
+@RequiredArgsConstructor
+public class ProfileService {
 
-    ProfileDto getProfile(String username);
+    private final UserRepository userRepository;
+    private final FollowRepository followRepository;
+    private final FollowJpaRepository followJpaRepository;
 
-    ProfileDto followUser(String username);
+    public ProfileDto getProfile(String username) {
+        User currentUser = getCurrentUser(getCurrentUserEmail());
+        User findUser = userRepository.findByUsername(username).orElseThrow(() -> new UserNotFoundException(username + " not found."));
+        boolean followed = followJpaRepository.isFollow(currentUser.getId(), findUser.getId());
+        return ProfileDto.of(findUser, followed);
+    }
 
-    ProfileDto unFollowUser(String username);
+    public ProfileDto followUser(String username) {
+        User currentUser = getCurrentUser(getCurrentUserEmail());
+        User findUser = userRepository.findByUsername(username).orElseThrow(() -> new UserNotFoundException(username + " not found."));
+        FollowRelation followRelation = new FollowRelation(currentUser.getId(), findUser.getId());
+        followRepository.save(followRelation);
+        return ProfileDto.of(findUser, true);
+    }
+
+    public ProfileDto unFollowUser(String username) {
+        User currentUser = getCurrentUser(getCurrentUserEmail());
+        User findUser = userRepository.findByUsername(username).orElseThrow(() -> new UserNotFoundException(username + " not found."));
+        followJpaRepository.unFollow(currentUser.getId(), findUser.getId());
+        return ProfileDto.of(findUser, false);
+    }
+
+    private String getCurrentUserEmail() {
+        return SecurityContextHolder.getContext().getAuthentication().getName();
+    }
+
+    private User getCurrentUser(String email) {
+        return userRepository.findByEmail(email).orElseThrow(() -> new UserNotFoundException(email + " not found."));
+    }
+
 }
