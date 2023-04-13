a8ebcb4fd847519687399ff40993b60794088741
ifi999 <kkwkjm4231@gmail.com>
2023-04-13 22:49:59 +0900

    refactor: Favorite Service Interface 제거

--- src/main/java/ifi/realworld/favorite/app/service/FavoriteService.java
+++ src/main/java/ifi/realworld/favorite/app/service/FavoriteService.java
@@ -1,10 +1,101 @@
 package ifi.realworld.favorite.app.service;
 
 import ifi.realworld.article.api.dto.SingleArticleDto;
+import ifi.realworld.article.domain.Article;
+import ifi.realworld.article.domain.ArticleTag;
+import ifi.realworld.article.domain.repository.ArticleRepository;
+import ifi.realworld.article.domain.repository.ArticleTagJpaRepository;
+import ifi.realworld.comment.domain.Comment;
+import ifi.realworld.comment.domain.repository.CommentRepository;
+import ifi.realworld.common.exception.api.AlreadyRegistArticleFavoriteException;
+import ifi.realworld.common.exception.api.ArticleNotFoundException;
+import ifi.realworld.common.exception.api.NotFoundArticleFavoriteRelationException;
+import ifi.realworld.common.exception.api.UserNotFoundException;
+import ifi.realworld.favorite.domain.Favorite;
+import ifi.realworld.favorite.domain.repository.FavoriteJpaRepository;
+import ifi.realworld.favorite.domain.repository.FavoriteRepository;
+import ifi.realworld.tag.domain.Tag;
+import ifi.realworld.user.domain.User;
+import ifi.realworld.user.domain.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
-public interface FavoriteService {
+import java.util.List;
+import java.util.stream.Collectors;
 
-    SingleArticleDto favoriteArticle(String slug);
+@Service
+@Transactional
+@RequiredArgsConstructor
+public class FavoriteService {
 
-    SingleArticleDto unfavoriteArticle(String slug);
+    private final FavoriteRepository favoriteRepository;
+    private final FavoriteJpaRepository favoriteJpaRepository;
+    private final UserRepository userRepository;
+    private final ArticleRepository articleRepository;
+    private final ArticleTagJpaRepository articleTagJpaRepository;
+    private final CommentRepository commentRepository;
+
+    public SingleArticleDto favoriteArticle(String slug) {
+        User currentUser = userRepository.findByEmail(getCurrentUserEmail()).orElseThrow(UserNotFoundException::new);
+        Article article = articleRepository.findBySlug(slug).orElseThrow(ArticleNotFoundException::new);
+        List<Comment> commentList = getCommentList(article);
+
+        Boolean favorited = favoriteJpaRepository.isFavorited(article.getId(), currentUser.getId());
+        if (favorited) throw new AlreadyRegistArticleFavoriteException("This " + article.getTitle() + " has already been favorited.");
+
+        Favorite favorite = Favorite.builder()
+                .articleId(article)
+                .userId(currentUser)
+                .build();
+        Favorite saved = favoriteRepository.save(favorite);
+        long favoriteCount = favoriteJpaRepository.articleFavoriteCount(article.getId());
+
+        return SingleArticleDto.builder()
+                .article(saved.getArticle())
+                .author(saved.getArticle().getAuthor())
+                .tagList(getTags(article.getId()))
+                .commentList(commentList)
+                .favorited(true)
+                .favoritesCount(favoriteCount)
+                .build();
+    }
+
+    public SingleArticleDto unfavoriteArticle(String slug) {
+        User currentUser = userRepository.findByEmail(getCurrentUserEmail()).orElseThrow(UserNotFoundException::new);
+        Article article = articleRepository.findBySlug(slug).orElseThrow(ArticleNotFoundException::new);
+        List<Comment> commentList = getCommentList(article);
+
+        Boolean favorited = favoriteJpaRepository.isFavorited(article.getId(), currentUser.getId());
+        if (!favorited) throw new NotFoundArticleFavoriteRelationException();
+
+        Favorite favorite = favoriteJpaRepository.findByArticleAndUser(article.getId(), currentUser.getId());
+        favoriteRepository.delete(favorite);
+        long favoriteCount = favoriteJpaRepository.articleFavoriteCount(article.getId());
+
+        return SingleArticleDto.builder()
+                .article(article)
+                .author(article.getAuthor())
+                .tagList(getTags(article.getId()))
+                .commentList(commentList)
+                .favorited(false)
+                .favoritesCount(favoriteCount)
+                .build();
+    }
+
+    private String getCurrentUserEmail() {
+        return SecurityContextHolder.getContext().getAuthentication().getName();
+    }
+
+    private List<Comment> getCommentList(Article article) {
+        return commentRepository.findByArticleId(article.getId());
+    }
+
+    private List<Tag> getTags(Long articldId){
+        List<ArticleTag> articleTags = articleTagJpaRepository.findByArticleId(articldId);
+        return articleTags.stream()
+                .map(o -> o.getTag())
+                .collect(Collectors.toList());
+    }
 }
