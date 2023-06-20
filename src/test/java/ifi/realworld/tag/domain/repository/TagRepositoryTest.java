a90e3e8266d25521a9c0110d23899266190f1221
ifi999 <kkwkjm4231@gmail.com>
2023-06-20 23:00:19 +0900

    test: TagRepository 테스트 코드 작성

---
+++ src/test/java/ifi/realworld/tag/domain/repository/TagRepositoryTest.java
@@ -0,0 +1,49 @@
+package ifi.realworld.tag.domain.repository;
+
+import ifi.realworld.TestSupport;
+import ifi.realworld.tag.domain.Tag;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.tuple;
+
+class TagRepositoryTest extends TestSupport {
+    
+    @DisplayName("태그명으로 조회한다.")
+    @Test
+    public void findByName() {
+        // given
+        Tag tag = new Tag("tag1");
+        tagRepository.save(tag);
+
+        // when
+        Tag getTag = tagRepository.findByName(tag.getName()).get();
+
+        // then
+        assertThat(getTag).isEqualTo(tag);
+        assertThat(getTag.getName()).isEqualTo(tag.getName());
+    }
+
+    @DisplayName("태그 목록을 조회한다.")
+    @Test
+    public void findNames() {
+        // given
+        Tag tag1 = new Tag("tag1");
+        Tag tag2 = new Tag("tag2");
+        Tag tag3 = new Tag("tag3");
+        tagRepository.saveAll(List.of(tag1, tag2, tag3));
+
+        // when
+        List<String> tags = tagRepository.findNames();
+
+        // then
+        assertThat(tags).hasSize(3)
+                .containsExactlyInAnyOrder(
+                        "tag1", "tag2", "tag3"
+                );
+    }
+
+}
